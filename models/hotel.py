7d8a1d65febc97ed74a4197132421530f7dc5663
Arthur Araujo <112352001+ArthurAraujo05@users.noreply.github.com>
2024-12-11 16:27:46 -0300

    Banco py feito

--- models/hotel.py
+++ models/hotel.py
@@ -1,4 +1,14 @@
-class HotelModel:
+from sql_alchemy import banco
+
+class HotelModel(banco.Model):
+    __tablename__= 'hoteis'
+
+    hotel_id = banco.Column(banco.String, primary_key=True)
+    name = banco.Column(banco.String(80))
+    estrelas = banco.Column(banco.Float(precision=1))
+    diaria = banco.Column(banco.Float(precision=2))
+    cidade = banco.Column(banco.String(40))
+
     def __init__(self, hotel_id, name, cidade, estrelas, diaria):
         self.hotel_id = hotel_id
         self.name = name
@@ -14,3 +24,25 @@ class HotelModel:
             'estrelas': self.estrelas,
             'diaria': self.diaria
         }
+
+    @classmethod
+    def find_hotel(cls, hotel_id):
+        hotel =  cls.query.filter_by(hotel_id=hotel_id).first()
+        if hotel:
+            return hotel
+        return None
+    
+    def save_hotel(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_hotel(self, name, cidade, estrelas, diaria):
+        self.name = name
+        self.cidade = cidade
+        self.estrelas = estrelas
+        self.diaria = diaria
+
+    def delete_hotel(self):
+        banco.session.delete(self)
+        banco.session.commit()
+    
--- resources/hotel.py
+++ resources/hotel.py
@@ -1,74 +1,56 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
 
-hoteis = [
-        {
-        'hotel_id':'victoria',
-        'name':'Victoria Resort',
-        'cidade': 'Campinas',
-        'estrelas': 5.0,
-        'diaria': 490.50
-        },
-        {
-        'hotel_id':'lisa',
-        'name':'Lisa Hotel',
-        'cidade' :'Vinhedo',
-        'estrelas': 4.2,
-        'diaria': 320.50
-        },
-        {
-        'hotel_id':'plazza',
-        'name':'Plazza Resort',
-        'cidade': 'hortolandia',
-        'estrelas': 2.0,
-        'diaria': 180.50
-        },
-]
-
 
 class Hoteis(Resource):
     def get(self):
-        return {'hoteis': hoteis}
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
     
 class Hotel(Resource):
     argumentos = reqparse.RequestParser()
-    argumentos.add_argument('name')
+    argumentos.add_argument('name', type=str, required=True, help="Campo 'nome' obrigatorio!")
+    argumentos.add_argument('estrelas', type=float, required=True, help="Campo 'estrelas' obrigatorio!")
     argumentos.add_argument('cidade')
-    argumentos.add_argument('estrelas')
     argumentos.add_argument('diaria')
-
-    def find_hotel(hotel_id):
-        for hotel in hoteis:
-            if hotel['hotel_id'] == hotel_id:
-                return hotel
-        return None   
+ 
             
     def get(self, hotel_id):
-        
-        hotel = Hotel.find_hotel(hotel_id)
+        hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            return hotel
+            return hotel.json()
         return {'message': 'Nenhum hotel encontrado.'} , 404
     
     def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'Erro interno do servidor'}, 500
+        return hotel.json()
         
     def put(self, hotel_id):
         dados = Hotel.argumentos.parse_args()
-        hotel_objeto = HotelModel(hotel_id, **dados)
-        novo_hotel = hotel_objeto.json()
-        hotel = Hotel.find_hotel(hotel_id)
-        if hotel:
-            hotel.update(novo_hotel)
-            return novo_hotel, 200
-        hoteis.append(novo_hotel)
-        return novo_hotel, 201
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'Erro interno do servidor'}, 500
+        return hotel.json(), 201
 
     def delete(self, hotel_id):
-        global hoteis
-        hoteis = [hotel for hotel in hoteis if hotel['hotel_id'] != hotel_id]
-        return {'message': 'Hotel deletado'}
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message': 'Erro interno do servidor'}, 500
+            return {'message': 'Hotel deletado'}
+        return{'message': 'Hotel nao encontrado.'}, 404
