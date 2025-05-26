0cf2cfcb2d23927a46ce1eeedd558ce0921c703e
Arthur Araujo <112352001+ArthurAraujo05@users.noreply.github.com>
2025-05-26 20:05:10 -0300

    commit add arquivos

--- resources/hotel.py
+++ resources/hotel.py
@@ -1,32 +1,9 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+from models.site import SiteModel
+from resources.filtros import normalize_path_params, consulta_com_cidade, consulta_sem_cidade
 from flask_jwt_extended import jwt_required
 import sqlite3
-
-def normalize_path_params(cidade=None,
-                          estrelas_min = 0,
-                          estrelas_max = 5,
-                          diaria_min = 0,
-                          diaria_max = 1000,
-                          limit = 50,
-                          offset = 0, **dados):
-                
-    if cidade:
-        return {
-            'estrelas_min': estrelas_min,
-            'estrelas_max': estrelas_max,
-            'diaria_min': diaria_min,
-            'diaria_max': diaria_max,
-            'cidade': cidade,
-            'limit': limit,
-            'offset': offset}
-    return {
-            'estrelas_min': estrelas_min,
-            'estrelas_max': estrelas_max,
-            'diaria_min': diaria_min,
-            'diaria_max': diaria_max,
-            'limit': limit,
-            'offset': offset}
 
 path_params = reqparse.RequestParser()
 path_params.add_argument('cidade', type=str)
@@ -41,86 +18,79 @@ class Hoteis(Resource):
     def get(self):
         connection = sqlite3.connect('banco.db')
         cursor = connection.cursor()
-        
 
         dados = path_params.parse_args()
         dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
         parametros = normalize_path_params(**dados_validos)
 
         if not parametros.get('cidade'):
-            consulta = "SELECT * FROM HOTEIS \
-            WHERE (estrelas > ? and estrelas < ?) \
-            and (diaria > ? and diaria < ?) \
-            LIMIT ? OFFSET ?"
-            tuple = tuple([parametros[chave] for chave in parametros])
-            resultado = cursor.execute(consulta, tuple)
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta_sem_cidade, tupla)
         else:
-            consulta = "SELECT * FROM HOTEIS \
-            WHERE (estrelas > ? and estrelas < ?) \
-            and (diaria > ? and diaria < ?) \
-            and cidade ? LIMIT ? OFFSET ?"
-            tuple = tuple([parametros[chave] for chave in parametros])
-            resultado = cursor.execute(consulta, tuple)
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta_com_cidade, tupla)
 
         hoteis = []
         for linha in resultado:
             hoteis.append({
-            'hotel_id': linha[0],
-            'name': linha[1],
-            'cidade': linha[2],
-            'estrelas': linha[3],
-            'diaria': linha[4]
+            'hotel_id': linha[0] ,
+            'nome': linha[1],
+            'estrelas': linha[2],
+            'diaria': linha[3],
+            'cidade': linha[4],
+            'site_id': linha[5]
             })
 
-        return {'hoteis': hoteis}
-    
+        return {'hoteis': hoteis} # SELECT * FROM hoteis
+
 class Hotel(Resource):
-    argumentos = reqparse.RequestParser()
-    argumentos.add_argument('name', type=str, required=True, help="Campo 'nome' obrigatorio!")
-    argumentos.add_argument('estrelas', type=float, required=True, help="Campo 'estrelas' obrigatorio!")
-    argumentos.add_argument('cidade')
-    argumentos.add_argument('diaria')
- 
-            
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be left blank.")
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
+    atributos.add_argument('site_id', type=int, required=True, help="Every hotel needs to be linked with a site.")
+
     def get(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
             return hotel.json()
-        return {'message': 'Nenhum hotel encontrado.'} , 404
-    
-    @jwt_required()
+        return {'message': 'Hotel not found.'}, 404
+
+    @jwt_required
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id):
-            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400
-        dados = Hotel.argumentos.parse_args()
+            return {"message": "Hotel id '{}' already exists.".format(hotel_id)}, 400 #Bad Request
+
+        dados = Hotel.atributos.parse_args()
         hotel = HotelModel(hotel_id, **dados)
+
+        if not SiteModel.find_by_id(dados['site_id']):
+            return {'message': 'The hotel must be associated to a valid site id.'}, 400
+
         try:
             hotel.save_hotel()
         except:
-            return {'message': 'Erro interno do servidor'}, 500
-        return hotel.json()
+            return {"message": "An error ocurred trying to create hotel."}, 500 #Internal Server Error
+        return hotel.json(), 201
 
-    @jwt_required()    
+    @jwt_required
     def put(self, hotel_id):
-        dados = Hotel.argumentos.parse_args()
+        dados = Hotel.atributos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+
         hotel_encontrado = HotelModel.find_hotel(hotel_id)
         if hotel_encontrado:
             hotel_encontrado.update_hotel(**dados)
             hotel_encontrado.save_hotel()
             return hotel_encontrado.json(), 200
-        hotel = HotelModel(hotel_id, **dados)
-        try:
-            hotel.save_hotel()
-        except:
-            return {'message': 'Erro interno do servidor'}, 500
+        hotel.save_hotel()
         return hotel.json(), 201
-    @jwt_required()
+
+    @jwt_required
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
-            try:
-                hotel.delete_hotel()
-            except:
-                return {'message': 'Erro interno do servidor'}, 500
-            return {'message': 'Hotel deletado'}
-        return{'message': 'Hotel nao encontrado.'}, 404
+            hotel.delete_hotel()
+            return {'message': 'Hotel deleted.'}
+        return {'message': 'Hotel not found.'}, 404
